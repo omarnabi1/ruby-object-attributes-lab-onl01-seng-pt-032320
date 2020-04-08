78a0d58d3e8cfd5e4361db540e977f576de37bed
Omar Nabi <onabi20@gmail.com>
2020-04-08 03:44:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,20 @@
 class Dog 
 
-def name=
+def name=(dogs_name)
+  @name = dogs_name
+end
+
+def name
   @name
+end
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+
+def breed
+  @breed
+end
+end
+
     
