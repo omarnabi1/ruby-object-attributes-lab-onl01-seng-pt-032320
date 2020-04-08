9e7040c7caa56e4774e5037d089dea6158dfb44b
Omar Nabi <onabi20@gmail.com>
2020-04-08 03:45:56 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(person_job)
+    @job = person_job
+  end
+  
+  def person_job
+    @person_job
+  end
+end
